9bbcd827ddc04c386909103966958b953b43543d
TahaAhmed63 <tahakap234@gmail.com>
2023-03-30 02:06:41 +0500

    first-commit

--- src/pages/details/Detail.jsx
+++ src/pages/details/Detail.jsx
@@ -5,9 +5,11 @@ import { useParams } from 'react-router-dom'
 import DetailBanner from './DetailBanner/DetailBanner'
 
 export const Detail = () => {
- // const {mediaType, id}=useParams()
-  //const {data, loading}=useFetch(`/${mediaType}/${id}`)
+ const {mediaType, id}=useParams()
+  const {data, loading}=useFetch(`/${mediaType}/${id}/videos`)
+  const {data:credits, loading:creditsLoading}=useFetch(`/${mediaType}/${id}/credits`)
+
   return (
-    <div><DetailBanner/></div>
+    <div><DetailBanner video={data?.results[0]} crew={credits?.crew}/></div>
   )
 }
